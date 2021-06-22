7dc09c40a930fb5ed5a464b49d25f85ba5b6a030
ttakku-park <ttakku.park@gmail.com>
2021-06-22 21:18:43 +0900

    CREATE : models.py 생성 , migration 완료

--- welcome2air/settings.py
+++ welcome2air/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'users',
+    'flights',
+    'tickets'
 ]
 
 MIDDLEWARE = [
