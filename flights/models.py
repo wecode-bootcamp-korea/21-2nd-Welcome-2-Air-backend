2e9c5400de2b0777189753159b0328b05e17f065
ttakku-park <ttakku.park@gmail.com>
2021-06-23 12:35:45 +0900

    FIX : modeling 수정 1. modeling 에러사항 수정완료, kakao id 추가

--- flights/models.py
+++ flights/models.py
@@ -15,32 +15,30 @@ class City(models.Model):
         db_table = 'cities'
 
 class Seat(models.Model):
-    name        = models.IntegerField()
+    name        = models.CharField(max_length=45)
     price_ratio = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         db_table = 'seats'
 
 class Flight(models.Model): 
-    flight_number     = models.CharField(max_length=20)
-    departure_date    = models.DateField()
-    arrival_date      = models.DateField()
-    departure_time    = models.TimeField()
-    arrival_time      = models.TimeField(max_length=20)
-    duration          = models.TimeField(max_length=20)
-    departure_city_id = models.ForeignKey(City, on_delete=models.CASCADE, related_name='departure')
-    arrival_city_id   = models.ForeignKey(City, on_delete=models.CASCADE, related_name='arrival')
-    price             = models.DecimalField(max_digits=18, decimal_places=2)
-    flight_seat       = models.ManyToManyField(Seat, through='FlightSeat')
+    flight_number      = models.CharField(max_length=20)
+    departure_datetime = models.DateTimeField()
+    arrival_datetime   = models.DateTimeField()
+    duration           = models.TimeField()
+    departure_city     = models.ForeignKey(City, on_delete=models.CASCADE, related_name='flight_departure_city')
+    arrival_city       = models.ForeignKey(City, on_delete=models.CASCADE, related_name='flight_arrival_city')
+    price              = models.DecimalField(max_digits=18, decimal_places=2)
+    flight_seat        = models.ManyToManyField(Seat, through='FlightSeat')
 
     class Meta:
         db_table = 'flights'
 
 class FlightSeat(models.Model):
 
-    flight_id = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='air_seat')
-    seat_id   = models.ForeignKey(Seat, on_delete=models.CASCADE)
-    stock     = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='flight_seat_flight')
+    seat   = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    stock  = models.IntegerField()
 
     class Meta:
         db_table = 'flight_seats'
