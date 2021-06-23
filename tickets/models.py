160b396784a74ab8b161040e8e4fe62565963d3f
ttakku-park <ttakku.park@gmail.com>
2021-06-23 12:35:45 +0900

    FIX : modeling 수정 1. modeling 에러사항 수정완료, kakao id 추가

--- tickets/models.py
+++ tickets/models.py
@@ -4,20 +4,21 @@ from users.models   import User
 from flights.models import Flight
 
 class Passenger(models.Model): 
-    name_kor = models.CharField(max_length=45)
-    name_eng = models.CharField(max_length=45)
-    birth    = models.DateField()
-    phone    = models.CharField(max_length=45)
-    email    = models.EmailField(max_length=100)
-    gender   = models.BooleanField()
-    ticket   = models.ManyToManyField(Flight, through='Ticket')
+    korean_name  = models.CharField(max_length=45)
+    english_name = models.CharField(max_length=45)
+    birth        = models.DateField()
+    phone        = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=100)
+    gender       = models.BooleanField()
+    passport     = models.CharField(max_length=45)
+    ticket       = models.ManyToManyField(Flight, through='Ticket')
 
     class Meta: 
         db_table = 'passengers'
 
 class Ticket(models.Model): 
-    user_id       = models.ForeignKey(User, on_delete=models.CASCADE)
-    passenger_id  = models.ForeignKey(Passenger, on_delete=models.CASCADE, related_name="air_ticket")
+    user          = models.ForeignKey(User, on_delete=models.CASCADE)
+    passenger     = models.ForeignKey(Passenger, on_delete=models.CASCADE, related_name="air_ticket")
     ticket_number = models.IntegerField()
     flight_id     = models.ForeignKey(Flight, on_delete=models.CASCADE)
 
