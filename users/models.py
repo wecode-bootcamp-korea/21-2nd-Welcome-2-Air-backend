e6999ee8ddacf1bf96e705282d93932e2c7e8c7b
ttakku-park <ttakku.park@gmail.com>
2021-06-23 12:35:45 +0900

    [FIX] : modeling 수정 1. modeling 에러사항 수정완료, kakao id 추가 2. requirements.txt 수정 3. migration 전체 삭제 후 다시생성 4. migrate 수정

--- users/models.py
+++ users/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 class User(models.Model): 
-    identity = models.CharField(max_length=45, unique=True)
-    password = models.CharField(max_length=45)
-    name_kor = models.CharField(max_length=45)
-    name_eng = models.CharField(max_length=45)
-    birth    = models.CharField(max_length=45)
-    phone    = models.CharField(max_length=45, unique=True)
-    email    = models.EmailField(max_length=100, unique=True)
-    gender   = models.BooleanField()
+    identity     = models.CharField(max_length=45, unique=True, null=True)
+    kakao_id     = models.CharField(max_length=45, unique=True, null=True)
+    password     = models.CharField(max_length=45)
+    korean_name  = models.CharField(max_length=45)
+    english_name = models.CharField(max_length=45)
+    birth        = models.CharField(max_length=45)
+    phone        = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=100)
+    gender       = models.CharField(max_length=45)
 
     class Meta: 
         db_table = 'users'
