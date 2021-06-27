5f6634de92777f54c463de11ec0e7ca686204600
ttakku-park <ttakku.park@gmail.com>
2021-06-27 15:22:55 +0900

    ADD : 항공편 조회 views.py 추가 1. 국가 리스트 로직 변경 2. Unit Test success

--- flights/tests.py
+++ flights/tests.py
@@ -1,3 +1,227 @@
-from django.test import TestCase
+from django.test      import TestCase, Client
 
-# Create your tests here.
+from .models  import Country, Flight, FlightSeat, Seat, City
+
+class FlightCountryListViewTest(TestCase):
+    def setUp(self):
+        client = Client() # 가상의 db
+
+        Country.objects.create(
+            id   = 1,
+            name = "대한민국"
+        )
+        City.objects.create(
+            id           = 1,
+            name         = "서울/김포",
+            airport_code = "GMP",
+            country_id   = 1
+        )
+        City.objects.create(
+            id           = 2,
+            name         = "제주",
+            airport_code = "CJU",
+            country_id   = 1
+        )
+        City.objects.create(
+            id           = 3,
+            name         = "부산/김해",
+            airport_code = "PUS",
+            country_id   = 1
+        )
+        City.objects.create(
+            id           = 4,
+            name         = "청주",
+            airport_code = "CJJ",
+            country_id   = 1
+        )
+
+    def tearDown(self) :
+        Country.objects.all().delete()
+        City.objects.all().delete()
+        Flight.objects.all().delete()
+        FlightSeat.objects.all().delete()
+        Seat.objects.all().delete()
+
+    def test_city_list(self) :
+        client = Client()
+        response = client.get('/flights/country')
+
+        self.assertEqual(response.json(),
+            {
+                "country_list": [
+                    {
+                        "대한민국": [
+                            {
+                                "city_id": 1,
+                                "city_name": "서울/김포",
+                                "airport_code": "GMP"
+                            },
+                            {
+                                "city_id": 2,
+                                "city_name": "제주",
+                                "airport_code": "CJU"
+                            },
+                            {
+                                "city_id": 3,
+                                "city_name": "부산/김해",
+                                "airport_code": "PUS"
+                            },
+                            {
+                                "city_id": 4,
+                                "city_name": "청주",
+                                "airport_code": "CJJ"
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+
+
+class FlightListViewTest(TestCase):
+    def setUp(self):
+        client = Client() # 가상의 db
+
+        Country.objects.create(
+            id   = 1,
+            name = "대한민국"
+        )
+        a = City.objects.create(
+            id           = 1,
+            name         = "서울/김포",
+            airport_code = "GMP",
+            country_id   = 1
+        )
+        b = City.objects.create(
+            id           = 2,
+            name         = "제주",
+            airport_code = "CJU",
+            country_id   = 1
+        )
+        Seat.objects.create(
+            id = 1,
+            name = 'economy',
+            price_ratio = 1.00,
+
+        )
+        Seat.objects.create(
+            id = 2,
+            name = 'business',
+            price_ratio = 1.50,
+
+        )
+        Seat.objects.create(
+            id = 3,
+            name = 'first',
+            price_ratio = 3.00,
+
+        )
+
+        Flight.objects.create(
+            id                 = 1,
+            flight_number      = "KA101",
+            departure_datetime = "2021-07-03 07:05",
+            arrival_datetime   = "2021-07-03 08:15",
+            duration           = "01:10",
+            departure_city     = b,
+            arrival_city       = a,
+            price              = 100000.00
+        )
+        Flight.objects.create(
+            id                 = 2,
+            flight_number      = "KA102",
+            departure_datetime = "2021-07-03 08:05",
+            arrival_datetime   = "2021-07-03 09:15",
+            duration           = "01:10",
+            departure_city     = b,
+            arrival_city       = a,
+            price              = 100000.00
+        )
+        FlightSeat.objects.create(
+            id        = 1,
+            stock     = 10,
+            flight_id = 1,
+            seat_id   = 1,
+        )
+        FlightSeat.objects.create(
+            id        = 2,
+            stock     = 8,
+            flight_id = 1,
+            seat_id   = 2,
+        )
+        
+        FlightSeat.objects.create(
+            id        = 3,
+            stock     = 5,
+            flight_id = 1,
+            seat_id   = 3,
+        )
+        FlightSeat.objects.create(
+            id        = 4,
+            stock     = 10,
+            flight_id = 2,
+            seat_id   = 1,
+        )
+        FlightSeat.objects.create(
+            id        = 5,
+            stock     = 8,
+            flight_id = 2,
+            seat_id   = 2,
+        )
+        FlightSeat.objects.create(
+            id        = 6,
+            stock     = 5,
+            flight_id = 2,
+            seat_id   = 3,
+        )
+
+    def tearDown(self) :
+        Country.objects.all().delete()
+        City.objects.all().delete()
+        Flight.objects.all().delete()
+        FlightSeat.objects.all().delete()
+        Seat.objects.all().delete()
+
+    def test_city_list(self) :
+        client = Client()
+        response = client.get('/flights?arrival_city_id=1&departure_city_id=2&arrival_datetime=2021-07-03&departure_datetime=2021-07-03&seat_name=first')
+
+        self.assertEqual(response.json(),
+            {
+                    "flights_view": [
+                        {
+                            "id": 1,
+                            "flight_id": 1,
+                            "flight_number": "KA101",
+                            "departure_datetime": "2021-07-03T07:05:00",
+                            "arrival_datetime": "2021-07-03T08:15:00",
+                            "duration": "01:10:00",
+                            "price": "100000.00",
+                            "departure_city_name": "제주",
+                            "arrival_city_name": "서울/김포",
+                            "departure_airport_code": "CJU",
+                            "arrival_airport_code": "GMP",
+                            "seat_stock": 5
+                        },
+                        {
+                            "id": 2,
+                            "flight_id": 2,
+                            "flight_number": "KA102",
+                            "departure_datetime": "2021-07-03T08:05:00",
+                            "arrival_datetime": "2021-07-03T09:15:00",
+                            "duration": "01:10:00",
+                            "price": "100000.00",
+                            "departure_city_name": "제주",
+                            "arrival_city_name": "서울/김포",
+                            "departure_airport_code": "CJU",
+                            "arrival_airport_code": "GMP",
+                            "seat_stock": 5
+                        }
+                    ]
+                }
+            )
+        self.assertEqual(response.status_code, 200)
+
+
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+import json, datetime
 
-# Create your views here.
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
+
+from .models  import Country, Flight
+
+class FlightCountryListView(View):
+    def get(self,request) :
+        result = []
+
+        for country in Country.objects.all() :
+            city_list = [{
+                    'city_id'     :city.id,
+                    'city_name'   :city.name,
+                    'airport_code':city.airport_code
+                } for city in country.city_set.all()]
+            result.append({country.name:city_list})
+        return JsonResponse({'country_list':result}, status=200)
+            
+class FlightListView(View):
+    def get(self,request) :
+
+        arrival_city_id    = request.GET.get('arrival_city_id',None)
+        departure_city_id  = request.GET.get('departure_city_id',None)
+        arrival_datetime   = request.GET.get('arrival_datetime',None)
+        departure_datetime = request.GET.get('departure_datetime',None)
+        seat_name          = request.GET.get('seat_name',None)
+        
+        today = datetime.datetime.now()
+
+        flight_filter = Flight.objects.filter(
+            Q(departure_city_id = departure_city_id)&
+            Q(arrival_city_id = arrival_city_id)&
+            Q(arrival_datetime__date = arrival_datetime)&
+            Q(departure_datetime__date = departure_datetime) &
+            Q(flight_seat_flight__seat__name = seat_name)&
+            Q(departure_datetime__gt = today)
+        )
+        print(flight_filter)
+        result=[
+                    {       
+                            'id'                    :index+1,
+                            'flight_id'             :flight.id ,
+                            'flight_number'         :flight.flight_number ,
+                            'departure_datetime'    :flight.departure_datetime,
+                            'arrival_datetime'      :flight.arrival_datetime,
+                            'duration'              :flight.duration,
+                            'price'                 :flight.price ,
+                            'departure_city_name'   :flight.departure_city.name ,
+                            'arrival_city_name'     :flight.arrival_city.name ,
+                            'departure_airport_code':flight.departure_city.airport_code ,
+                            'arrival_airport_code'  :flight.arrival_city.airport_code ,
+                            'seat_stock'            :flight.flight_seat_flight.get(seat__name=seat_name).stock,
+                    } for index,flight in enumerate(flight_filter)]
+
+        return JsonResponse({'flights_view' :result}, status=200)
