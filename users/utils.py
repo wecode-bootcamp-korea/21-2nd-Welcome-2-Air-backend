0979848fb35f55144a2de5ef0d1aa1654c9f9779
이정민 <dlwjdals5600@gmail.com>
2021-07-02 09:59:31 +0900

    add : template html 레이아웃 추가

--- users/utils.py
+++ users/utils.py
@@ -10,7 +10,7 @@ def login_decorator(func):
         try:
             token        = request.headers.get('Authorization', None)
             payload      = jwt.decode(token, SECRET_KEY, ALGORITHM)
-            user         = User.objects.get(id=payload['id'])
+            user         = User.objects.get(id=payload['user_id'])
             request.user = user
 
             return func(self, request, *args, **kwargs)
